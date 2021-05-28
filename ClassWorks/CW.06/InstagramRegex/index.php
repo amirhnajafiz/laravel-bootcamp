176348448ee1e4b366a5b3cd2fad923208338534
AmirH.Najafizadeh <najafizadeh21@gmail.com>
2021-05-28 11:12:42 +0430

    Update index.php

--- ClassWorks/CW.06/InstagramRegex/index.php
+++ ClassWorks/CW.06/InstagramRegex/index.php
@@ -14,3 +14,43 @@
 		return $input_string;
 	}
 ?>
+
+<?php
+	function generateRandomString($length = 10) {
+		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$charactersLength = strlen($characters);
+		$randomString = '';
+		for ($i = 0; $i < $length; $i++) {
+			$randomString .= $characters[rand(0, $charactersLength - 1)];
+		}
+		return $randomString;
+	}
+	
+	function program_test($number_of_tests = 5)
+	{
+		for($i = 1; $i <= $number_of_tests; $i++)
+		{
+			$string_length = rand(5, 20);
+			$testing_string = "";
+			for($j = 0; $j < $string_length; $j++)
+			{
+				$temp = generateRandomString(rand(5,20));
+				$prop = rand(0, 9);
+				if ($prop > 7)
+				{
+					$temp = rand(0,9) > 5 ? '#' . $temp : '@' . $temp;
+				}
+				$testing_string = $testing_string . " " . $temp;
+			}
+			echo "<br /> Test Case $i : <br />";
+			echo ">> Input string <br /> $testing_string <br />";
+			echo ">> Output string <br /> " . clear_tags($testing_string) . "<br />";
+		}
+	}
+?>
+
+<?php
+	echo "This function gets a text with '@' and '#' init and then converts them to links.<br />";
+	$number_of_tests = 12;
+	program_test($number_of_tests);
+?>
