38aa437f212c9e2e16b973e0b578227413ec25aa
9831065 <najafizadeh21@gmail.com>
2021-06-15 19:18:42 +0430

    CW10 Page View added

---
+++ ClassWorks/CW.10/2.Page View/blank.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+   
+    if(isset($_SESSION['views_blank']))
+        $_SESSION['views_blank'] = $_SESSION['views_blank'] + 1;
+    else
+        $_SESSION['views_blank'] = 1;
+
+?>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+            integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    </head>
+    <body>
+        <button type="button" class="btn btn-primary d-block" style="display: block;">Views: <span class="badge"><?php echo $_SESSION['views_blank'] ?></span><button>
+        <ul class="list-group">
+            <li class="list-group-item">
+                <a href="index.php">Home</a>
+            </li>
+            <li class="list-group-item">
+                <a href="contact.php">Contact</a>
+            </li>
+        </ul>
+    </body>
+</html>
